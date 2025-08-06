f19659e891ae8aae86379698af68951cb0f7c934
Santu Mondal <mondalsantu104@gmail.com>
2025-08-06 19:41:19 +0530

    initialize node & undderstand module

---
+++ 01-Hello.js
@@ -0,0 +1,8 @@
+console.log("Hey there");
+
+ const math = require("./Math")
+
+console.log("Value of the addition:" , math.add(2, 4));
+console.log("Value of the subtraction:", math.sub(6, 3));
+
+
