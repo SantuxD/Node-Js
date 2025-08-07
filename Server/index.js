45cd3011a69aeb5977f7249a5a2a3a9c82729b13
Santu Mondal <mondalsantu104@gmail.com>
2025-08-07 20:47:28 +0530

    install custom url package and learn how to work it

--- Server/index.js
+++ Server/index.js
@@ -1,34 +1,41 @@
 const server = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const myServer = server.createServer((req, res) => {
-    const log = `${Date.now()} ${req.url} ${req.headers}: new req received\n`
-    fs.appendFile("./log.txt", log, (err, data) => {
-        // if(err){
-        //     console.log("Error found:", err)
-        // }else{
-        //     console.log("Received Data:", data);
-        // }
+  if (req.url === "/favicon.ico") return res.end();
+  const log = `${Date.now()} ${req.url} : new req received\n`;
+  const myurl = url.parse(req.url, true);
+  console.log(myurl);
+  
+  fs.appendFile("./log.txt", log, (err, data) => {
+    // if(err){
+    //     console.log("Error found:", err)
+    // }else{
+    //     console.log("Received Data:", data);
+    // }
 
+    switch (myurl.pathname) {
+      case "/":
+        res.end("Welcome to home page");
+        break;
+      case "/contactMe":
+        res.end("Contact me");
+        break;
+      case "/about":
+        const userName = myurl.query.myname;
+        res.end(`Hello my name is, ${userName}`);
+        break;
 
-        switch (req.url) {
-            case "/":
-                res.end("Welcome to home page")
-                break;
-            case "/contactMe":
-                res.end("Contact me")
-                break;
-            case "/about":
-                res.end("About me")
-                break;
-            default:
-                res.end("404 Not Found")
+      case "/search":
+        const result = myurl.query.search_query;
+        res.end(`result is, ${result}`);
+        break;
 
-        }
-        
-    })
-
-
+      default:
+        res.end("404 Not Found");
+    }
+  });
 });
 
-myServer.listen(8000, () => console.log('Server started'))
+myServer.listen(8000, () => console.log("Server started"));
